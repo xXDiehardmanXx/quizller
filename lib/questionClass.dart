ef30aaf48f4cf8ea35bb3bb3ec1d472420d60d0b
root <rakshitvermajpr@gmail.com>
2021-05-16 12:20:03 +0530

    added questionClass.dart and quizBrain.dart

---
+++ lib/questionClass.dart
@@ -0,0 +1,9 @@
+class Question{
+  String questionText;
+  bool questionAnswer;
+
+  Question({String que, bool ans}){
+    questionText = que;
+    questionAnswer = ans;
+  }
+}
---
+++ lib/quizBrain.dart
@@ -0,0 +1,9 @@
+import'package:quizller/questionClass.dart';
+
+class QuizBrain{
+  List<Question> questionBank = [
+    Question(que:'You can lead a cow down stairs but not up stairs.', ans: false),
+    Question(que:'Approximately one quarter of human bones are in the feet.', ans: true),
+    Question(que:'A slug\'s blood is green.', ans: true),
+  ];
+}
